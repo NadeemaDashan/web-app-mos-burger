42fe508316ce588f8c9d4060e3b002130a553ace
Dashan Nadeema <dashanwd@gmail.com>
2023-12-12 22:28:12 +0530

    dashboard completed

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const adminArray = [];
+let admin = {username:"Dashan",password:"1234"};
+adminArray.push(admin);
+document.getElementById("btn-log-in").onclick=function (){
+    if (document.getElementById("fld-employee-id").value===admin.username && document.getElementById("fld-password").value===admin.password){
+
+    }
+
+}
+
