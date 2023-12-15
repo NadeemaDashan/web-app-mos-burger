c16775718714f870459f382420faaee41b806d4a
Dashan Nadeema <dashanwd@gmail.com>
2023-12-15 20:42:48 +0530

    customer read, update, delete completed

--- js/app.js
+++ js/app.js
@@ -64,7 +64,7 @@ function customerSignUp() {
     }else {
         alert("Password length should be more than 5 characters")
     }
-    window.location="../html/customerEdit.html"
+
 
 }
 //customer sign up page clear
@@ -82,8 +82,68 @@ function customerClear(){
     countryCode.value="";
 }
 
-//customer info edit page
+//customer info load
+function customerLoad(){
+    let customerArray = JSON.parse(localStorage.array);
+    let customerIdTxt=document.getElementById("fld-search-customer").value
+
+    let customerName=document.getElementById("fld-full-name")
+    let customerIdAs =document.getElementById("fld-contact-no")
+    let customerEmail=document.getElementById("fld-email")
+    let customerPassword=document.getElementById("fld-password")
+    let countryCode=document.getElementById("fld-country-code")
+    let isExist = false;
+    for (let i = 0; i <customerArray.length ; i++) {
+        if (customerIdTxt===customerArray[i].customerId) {
+            customerName.value = customerArray[i].username
+            customerIdAs.value = customerArray[i].customerId
+            customerEmail.value = customerArray[i].customerEmail
+            customerPassword.value = customerArray[i].customerPassword
+            countryCode.value = "+94"
+            isExist=true
+        }
+    }
+    if (isExist===false){
+        customerName.value = "INVALID CUSTOMER ID"
+        customerIdAs.value = "INVALID CUSTOMER ID"
+        customerEmail.value = "INVALID CUSTOMER ID"
+        countryCode.value = "+94"
+    }
+
+}
+
+// customer info edit
 function customerEdit(){
     let customerArray = JSON.parse(localStorage.array);
-    
+    let customerName=document.getElementById("fld-full-name").value
+    let customerIdAs =document.getElementById("fld-contact-no").value
+    let customerEmail=document.getElementById("fld-email").value
+    let customerPassword=document.getElementById("fld-password").value
+
+    for (let i = 0; i < customerArray.length; i++) {
+        if (customerArray[i].customerId===customerIdAs){
+            customerArray.splice(i);
+            let customer ={username:customerName,customerId:customerIdAs,customerEmail:customerEmail,
+                customerPassword:customerPassword}
+            customerArray.push(customer);
+        }
+    }
+    localStorage.array=JSON.stringify(customerArray);
+
+}
+// customer info Delete
+function customerDelete(){
+    let customerArray = JSON.parse(localStorage.array);
+    let customerName=document.getElementById("fld-full-name").value
+    let customerIdAs =document.getElementById("fld-contact-no").value
+    let customerEmail=document.getElementById("fld-email").value
+    let customerPassword=document.getElementById("fld-password").value
+
+    for (let i = 0; i < customerArray.length; i++) {
+        if (customerArray[i].customerId === customerIdAs) {
+            customerArray.splice(i);
+        }
+    }
+    localStorage.array=JSON.stringify(customerArray);
+
 }
